5a7da91e81a4066fbfd09db881a5a23498fc428b
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-04-15 16:56:59 +0200

    custom size

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 !function ($) {
 		$('iframe').load(function () {
-			var width = $(".content-width").width();
-			var height = $(".content-height").height();
-			console.log(width);
-			console.log(height);
-			$(".content-width").val(width);
-			$(".content-height").val(height);
+			var iframe = $('iframe');
+			var ipt_width = $("#content-width");
+			var ipt_height = $("#content-height");
+			var slider_width = 25;
+
+			// Get iframe size
+			ipt_width.val(iframe.width() - slider_width);
+			ipt_height.val(iframe.height() - slider_width);
+
+			// Set iframe size
+			ipt_width.focusout(function() {
+				iframe.animate({width: parseInt(ipt_width.val()) + slider_width});
+			});
+			ipt_height.focusout(function() {
+				iframe.animate({height: parseInt(ipt_height.val()) + slider_width});
+			});
 		});
 }(window.jQuery);
