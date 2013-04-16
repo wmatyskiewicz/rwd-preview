fafd71c64ac95802e937f9c6cd018a2274358673
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-04-16 15:20:42 +0200

    added slider

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
 !function ($) {
-		$('iframe').load(function () {
-			var iframe = $('iframe');
-			var ipt_width = $("#content-width");
-			var ipt_height = $("#content-height");
-			var slider_width = 25;
+	var iframe = $('iframe');
+	var ipt_width = $("#content-width");
+	var ipt_height = $("#content-height");
+	var slider_width = 25;
 
-			// Get iframe size
-			ipt_width.val(iframe.width() - slider_width);
-			ipt_height.val(iframe.height() - slider_width);
-
-			// Set iframe size
-			ipt_width.focusout(function() {
-				iframe.animate({width: parseInt(ipt_width.val()) + slider_width});
-			});
-			ipt_height.focusout(function() {
-				iframe.animate({height: parseInt(ipt_height.val()) + slider_width});
-			});
+	function iframe_resizer(iframe, width, height) {
+		width.focusout(function() {
+			iframe.animate({width: parseInt(width.val()) + slider_width});
 		});
+		height.focusout(function() {
+			iframe.animate({height: parseInt(height.val()) + slider_width});
+		});
+	}
+
+	$('iframe').load(function () {
+		ipt_width.val(iframe.width() - slider_width);
+		ipt_height.val(iframe.height() - slider_width);
+		iframe_resizer(iframe, ipt_width, ipt_height);
+	});
+
+	$('#pixel-slider').slider({
+		range: "max",
+		min: 320,
+		max: 1600,
+		value: 1024,
+		orientation: "horizontal",
+		animate: true,
+		slide: function(event, ui) {
+			ipt_width.val(ui.value);
+		}
+	});
 }(window.jQuery);
